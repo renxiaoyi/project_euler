66ebf6c88f196bf602d7be3bf104dadad13604fc
renxiaoyi <renxiaoyi1986@gmail.com>
2015-07-31 09:56:29 +0800

    problem 129

---
+++ problem_129.py
@@ -0,0 +1,23 @@
+def Undividable(n, m):
+  # Returns True if n divides none of {1, 11, 111, ... R(m)}.
+  rems = set([1])  # remainders {1, 11, 111, ... R(m)} % n
+  r = 1  # remainder 10^i % n
+  rem = 1 # remainder R(i) % n
+  for i in xrange(2, m+1):
+    r = 10*r % n
+    rem = (rem+r) % n
+    if rem == 0 or rem in rems:
+      return False
+    rems.add(rem)
+  return True
+
+def LeastN(m):
+  n = m
+  while True:
+    n += 1
+    if n % 2 == 0 or n % 5 == 0:
+      continue
+    if Undividable(n, m):
+      return n
+
+print LeastN(10**6)
